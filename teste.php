77311ae4b8cf7860a06a12ead036ce82eaf56ed8
Dev-JorgeII <junior.jjx@gmail.com>
2024-09-26 20:15:08 -0300

    Portfolio Desatualizado2

---
+++ teste.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    /*criando aqui as variaveis a e b e atribuindo valor a elas*/
+        $a = 10;
+        $b = 15;
+    //aqui criamos a variavel c
+        $c = $a + $b;
+    /*escrevemos em tela o valor das variaveis*/
+        echo "$a mais $b e igual a $c";
+
+    ?>
+    
+</body>
+</html>
---
+++ teste2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a = 25;
+        $b = 5;
+
+        echo "A soma de $a e $b e igual a ".($a + $b)."<br>";
+        echo "A subtração de $a e $b e igual a ".($a - $b)."<br>";
+        echo "A multiplicação de $a e $b e igual a ".($a * $b)."<br>";
+        echo "A divisão de $a e $b e igual a ".($a / $b)."<br>";
+        echo "A exponenciação de $a e $b e igual a ".($a ** $b)."<br>";
+        echo "O resto de $a e $b e igual a ".($a % $b)."<br>";
+    ?>
+    
+</body>
+</html>
